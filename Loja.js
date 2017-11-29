35886780c1e4b3e75cfb873e736f70b83905399a
José Carlos Emídio Pereira <carlosthebest@A305-15L.imd.local>
2017-11-29 18:22:10 -0300

    add class Produto

---
+++ Loja.js
@@ -0,0 +1,9 @@
+/* Objeto do tipo jogador */
+function Produto(_codigo, _foto, _nome, _descricao, _preco, _peso) {
+	this.codigo = _codigo;
+	this.foto = _foto;
+	this.nome = _nome;
+	this.descricao = _descricao; 
+	this.preco = _preco;
+	this.peso = _peso;
+}
